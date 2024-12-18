cd41b9834c97f3cd3285da541abe4860cb86b53c
Kornii0702 <kornel.myczko@gmail.com>
2024-12-18 15:32:44 +0100

    Update ListGroup.tsx

--- react-app-UZ/src/components/ListGroup.tsx
+++ react-app-UZ/src/components/ListGroup.tsx
@@ -1,11 +1,11 @@
 function ListGroup() {
   return (
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+      <li className="list-group-item">1</li>
+      <li className="list-group-item">2</li>
+      <li className="list-group-item">3</li>
+      <li className="list-group-item">4</li>
+      <li className="list-group-item">5</li>
     </ul>
   );
 }
