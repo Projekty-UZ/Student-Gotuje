538eee980da0d3e2947bff15a7f9f63cde6bbc6a
Kornii0702 <kornel.myczko@gmail.com>
2024-12-19 09:43:07 +0100

    .

--- react-app-UZ/src/App.tsx
+++ react-app-UZ/src/App.tsx
@@ -1,9 +1,11 @@
 import LoginFrom from "./components/LoginForm/LoginForm";
-
+import LoginButton from "./components/LoginForm/LoginButton";
+import "./App.css";
 function App() {
   return (
     <div>
       <LoginFrom />
+      <LoginButton>Zaloguj sie</LoginButton>
     </div>
   );
 }
---
+++ react-app-UZ/src/components/LoginForm/LoginButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LoginButton = (props: Props) => {
+  return <button {...props} />;
+};
+
+export default LoginButton;
